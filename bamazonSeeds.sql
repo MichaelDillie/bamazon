f58485b2824d9eb62a16769715f4c62e05adb7b7
Michael Dillie <michaeldillie@gmail.com>
2018-05-18 16:38:37 -0500

    Added mock data

--- bamazonSeeds.sql
+++ bamazonSeeds.sql
@@ -14,4 +14,31 @@ CREATE TABLE products(
 );
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("fish", "animals", 2, 100);
+VALUES ("goldfish", "animals", 2, 100);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("soccer ball", "sports", 15, 500);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("football", "sports", 10, 900);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("ping pong balls", "spots", 8, 600);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("laptop", "electronics", 1300, 5000);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("headphones", "electronics", 30, 1500);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("google pixel", "electronics", 500, 3000);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("golf ball", "sports", 20, 10000);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("tablet", "electronics", 350, 1000);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("lawn darts set", "sports", 35, 200);
