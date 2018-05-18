45dd7421113d9e68943c883b2484094cf4606e69
Michael Dillie <michaeldillie@gmail.com>
2018-05-18 17:07:36 -0500

    Now able to read from products table in the DB

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,9 +3,22 @@ var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
   host: "localhost",
-  prot: 8889,
+  port: 8889,
   user: "root",
   password: "root",
   database: "bamazon_db"
 });
 
+connection.connect(function(err) {
+  if(err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  readProducts();
+});
+
+function readProducts() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if(err) throw err;
+    console.log(res);
+    connection.end();
+  });
+}
