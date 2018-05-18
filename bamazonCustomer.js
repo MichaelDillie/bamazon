21696ccd9892e083b27354722445370b21727c98
Michael Dillie <michaeldillie@gmail.com>
2018-05-18 16:53:24 -0500

    Created connection with mysql

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,11 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  prot: 8889,
+  user: "root",
+  password: "root",
+  database: "bamazon_db"
+});
+
