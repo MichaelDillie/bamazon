a98790a0927d5d9762d8c9b27dd0b32334454c7c
Michael Dillie <michaeldillie@gmail.com>
2018-05-18 16:22:13 -0500

    Fixed a problem (Forgot to add PRIMARY KEY). Also changed js file name to bamazonCustomer

--- bamazonSeeds.sql
+++ bamazonSeeds.sql
@@ -4,10 +4,14 @@ CREATE DATABASE bamazon_db;
 
 USE bamazon_db;
 
-CREATE TABLE products (
+CREATE TABLE products(
   id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(50),
   department_name VARCHAR(30),
   price INT NULL,
-  stock_quantity INT NULL
+  stock_quantity INT NULL,
+  PRIMARY KEY (id)
 );
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("fish", "animals", 2, 100);
