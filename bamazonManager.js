a0f3bf785e3fc62b853fa6ef366f5d49c2793598
Michael Dillie <michaeldillie@gmail.com>
2018-05-18 21:58:08 -0500

    Fixed up the look of the productsForSale function

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,4 +1,4 @@
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
@@ -9,12 +9,62 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   start();
 })
 
 function start() {
-  console.log("Hello World!");
-  connection.end();
+  inquirer
+    .prompt([
+      {
+      type: "list",
+      name: "choice",
+      choices: [
+        "View Products for Sale",
+        "View Low Inventory",
+        "Add to Inventory",
+        "Add New Product"
+      ],
+      message: "Select an option"
+    }
+  ])
+    .then(function (answers) {
+      switch (answers.choice) {
+        case "View Products for Sale":
+          productsForSale();
+          break;
+        case "View Low Inventory":
+          // lowInventory();
+          break;
+        case "Add to Inventory":
+          // addInventory();
+          break;
+        case "Add New Product":
+          // newProduct();
+          break;
+      }
+    });
+}
+
+function productsForSale() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if(err) throw err;
+    for(var i = 0; i < res.length; i++) {
+      console.log("---------------------------------");
+      console.log("Product ID - " + res[i].id);
+      console.log("Product Name - " + res[i].product_name);
+      console.log("Product Price - " + res[i].price);
+      console.log("Units in Stock - " + res[i].stock_quantity);
+    }
+    connection.end();
+  });
+}
+
+function lowInventory() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    for(var i = 0; i < res.length; i++) {
+      console.log(res[i].stock_quantity)
+    }
+  });
 }
