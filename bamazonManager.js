f489d4d28462f3b869aaa26dbc54b3adcc236a12
Michael Dillie <michaeldillie@gmail.com>
2018-05-18 21:31:02 -0500

    Created the manager file with the basics in it

---
+++ bamazonManager.js
@@ -0,0 +1,20 @@
+var mysql      = require('mysql');
+var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 8889,
+  user: "root",
+  password: "root",
+  database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  start();
+})
+
+function start() {
+  console.log("Hello World!");
+  connection.end();
+}
