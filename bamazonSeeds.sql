008bbe17d7c511e15085ba5b54e84fc904b90caf
Michael Dillie <michaeldillie@gmail.com>
2018-05-20 10:33:07 -0500

    Added a new column to departments table

--- bamazonSeeds.sql
+++ bamazonSeeds.sql
@@ -20,6 +20,7 @@ CREATE TABLE departments(
   department_id INT NOT NULL AUTO_INCREMENT,
   department_name VARCHAR(30),
   over_head_cost INT NULL,
+  prodcut_sales INT DEFAULT 0,
   PRIMARY KEY (department_id)
 );
 
